eef64a96eb542770d755e921bdd8ebaa9643f654
LBeul <l.beul@outlook.de>
2022-01-30 00:50:16 +0100

    adjust padding

--- src/components/style/ContentBox.tsx
+++ src/components/style/ContentBox.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components"
 const ContentBox = styled.div`
   background-color: #202121;
   border-radius: 20px;
-  padding: 20px;
+  padding: 40px;
   display: flex;
   width: 40vw;
   flex-direction: column;
