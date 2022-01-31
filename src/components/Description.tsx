7e43332a252fb7dc559f4b01e51173cd63883c5f
LBeul <l.beul@outlook.de>
2022-01-31 11:45:20 +0100

    Fix typos

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -5,14 +5,19 @@ const Description: FC = () => (
   <StyledDiv>
     <h1 style={{ textAlign: "center" }}>Welcome to Plover</h1>
     <h3 className="description-heading">PlaylistsCovers made easy</h3>
-    Did yo ever create a spotify playlist and thought{" "}
+    Did you ever create a spotify playlist and thought{" "}
     <em>"Damn, that generically generated cover looks dope as hell" </em>? Yeah,
     me neither.{" "}
     <strong>
-      Your playlist's cover should be as individual as the music you listen to
+      Your playlist's cover should be as individual as the music you listen to.
     </strong>
-    . Simply type enter a fitting image caption and your playlist's name - et
-    voila: plover creates an awesome cover for you.
+    <br />
+    <br />
+    So, say Hello to <strong>Plover</strong>! Just enter a keyword for the image
+    search and your playlist's name - et voila: plover creates an awesome cover
+    for you. Don't like the chosen image? No worries, hit the <em>Generate!</em>{" "}
+    button once more to get a random new image that corresponds to your keyword
+    search!
   </StyledDiv>
 )
 
